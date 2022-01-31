33a1dadf2e4c8afe9bac679e2d962144c0ad1851
Noah Taggart <noahtag@gmail.com>
2022-01-31 13:14:07 -0600

    listing past requests

---
+++ src/scripts/Requests.js
@@ -0,0 +1,19 @@
+import { getRequests } from "./dataAccess.js"
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    let html = `
+        <ul>
+            ${
+                requests.map(convertRequestToListElement).join("")
+            }
+        </ul>
+    `
+
+    return html
+}
+
+const convertRequestToListElement = (request) => {
+    return `<li>${request.description}</li>`
+}
--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,4 +1,25 @@
 const applicationState = {
+    requests: [],
 
+
+
+}
+
+export const getRequests = () => {
+    return applicationState.requests.map(request => ({ ...request}))
+}
+
+
+const API = "http://localhost:8088"
+
+export const fetchRequests = () => {
+    return fetch(`${API}/requests`)
+        .then(response => response.json())
+        .then(
+            (serviceRequests) => {
+                // Store the external state in application state
+                applicationState.requests = serviceRequests
+            }
+        )
 }
 
